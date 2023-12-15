57d92b6d326926374692b23290a2894706033af4
mosaert <raiacosta13@gmail.com>
2023-12-15 15:48:24 +0100

    UsuarioController anyadidos

---
+++ src/Controller/UsuarioController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class UsuarioController extends AbstractController
+{
+    public function index(){
+        
+    }
+}
